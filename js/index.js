e9bf3c05324faaf545db37f4de3a8c7425647465
Cired1 <edabundis@gmail.com>
2021-12-23 14:23:24 -0600

    Fix layout

--- js/index.js
+++ js/index.js
@@ -1,5 +1,70 @@
+
+//Proyectos
+const data = [
+  {
+    name: "FilmTv",
+    description: "Página web en el cual se encuentra información detallada de una gran variedad de películas y series. Creada con React y TMDB Api",
+    path_image: "assets/filmtv.png",
+    tech: ["HTML5", "CSS3", "JavaScript", "React", "TMDB API"],
+    demo: "https://filmtv-cired.netlify.app/",
+    code: "https://github.com/Cired1/filmtv",
+  },
+  {
+    name: "Blog",
+    description: "Blog creado con React donde se muestran artículos a traves de la API de Wordpress.",
+    path_image: "assets/blog-css.png",
+    tech: ["HTML5", "CSS3", "JavaScript", "React", "Wordpress API"],
+    demo: "https://blog-css.netlify.app/",
+    code: "https://github.com/Cired1/blog-react",
+  },
+  {
+    name: "Blog",
+    description: "Blog creado con React donde se muestran artículos a traves de la API de Wordpress.",
+    path_image: "assets/blog-css.png",
+    tech: ["HTML5", "CSS3", "JavaScript", "React", "TMDB API"],
+    demo: "https://blog-css.netlify.app/",
+    code: "https://github.com/Cired1/blog-react",
+  },
+];
+
+//Funciones
+
+const loadProjects = () => {
+  const $container = document.getElementById("projects");
+  const $template = document.getElementById("template-project").content;
+  const $fragment = document.createDocumentFragment();
+
+  data.map((project) => {
+    $template.querySelector(".project-image").src = project.path_image;
+    $template.querySelector(".project-image").alt = project.name;
+    $template.querySelector(".project-title").textContent = project.name;
+    $template.querySelector(".project-description").textContent =
+      project.description;
+
+    let skillList = "";
+    project.tech.forEach((skill) => {
+      skillList += `<li>${skill}</li>`;
+    });
+    $template.querySelector(".project-skill-list").innerHTML = skillList;
+
+    let codeLink = `<li><a href=${project.code} target="_blank" rel="noopener"><i
+    class="fab fa-github"></i> Codígo</a></li>`;
+    let demoLink = `<li><a href=${project.demo} target="_blank" rel="noopener"><i
+    class="fas fa-external-link-alt"></i> Demo</a></li>`;
+    $template.querySelector(".project-links").innerHTML = codeLink + demoLink;
+
+    let $clone = document.importNode($template, true);
+    $fragment.appendChild($clone);
+    $container.appendChild($fragment);
+  });
+};
+
+
+//Eventos
+
 document.addEventListener("DOMContentLoaded", (e) => {
   document.documentElement.setAttribute("data-theme", "light");
+  loadProjects();
 });
 
 document.addEventListener("click", (e) => {
@@ -28,3 +93,4 @@ window.addEventListener("scroll", (e) => {
     document.querySelector(".btn-return").classList.add("none");
   }
 });
+
